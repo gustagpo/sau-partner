2cb54796ffc01cb81e741ba9d1079210f1984a97
GP Soluções <gustavo@teamgp.com.br>
2023-08-08 05:51:59 -0300

    ajsute axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 const token = Cookies.get("@sau_benefits_token");
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333/api/v1/",
+  baseURL: "https://ws.saubeneficios.com.br/api/v1/",
 
   headers: {
     Authorization: `Bearer ${token}`,
